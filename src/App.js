a1ede7c76119063afe7b588d677500a57305f2dc
knowuchiha <knowuchiha2881@gmail.com>
2021-02-27 14:45:04 +0530

    added todo for reading list

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, {Component}  from 'react';
+import Navbar              from './components/Navbar';
+import BookList            from './components/BookList';
+import BookForm            from './components/BookForm';
+import BookContextProvider from './context/BookContext';
+
+class App extends Component {
+	constructor(props){
+		super(props)
+	}
+
+	render() {
+		return(
+			<div className='App'>
+				<BookContextProvider>
+					<Navbar/>
+					<BookList/>
+					<BookForm/>
+				</BookContextProvider>
+			</div>
+			);
+	}
+}
+
+export default App;
---
+++ src/reducer/bookReducer.js
@@ -0,0 +1,14 @@
+export const bookReducer = (state, action) => {
+	
+	switch (action.type) {
+		case 'ADD_BOOK' :
+			return [...state,action.book]
+
+		case 'REMOVE_BOOK' :
+			return state.filter(book => book.id !== action.id)
+
+		default:
+			return state
+
+	}
+}
